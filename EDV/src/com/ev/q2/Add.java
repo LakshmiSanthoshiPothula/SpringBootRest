11d92e090aad9e222f89d46a14e45cc0745d8d8e
LakshmiSanthoshiPothula <44640482+LakshmiSanthoshiPothula@users.noreply.github.com>
2021-01-22 17:28:43 +0530

    sdS

--- EDV/src/com/ev/q2/Add.java
+++ EDV/src/com/ev/q2/Add.java
@@ -1,5 +1,22 @@
 package com.ev.q2;
 
-public class Add {
-
-}
+import java.util.StringTokenizer;  
+public class Add{  
+ public static void main(String args[]){  
+   StringTokenizer st = new StringTokenizer("I am     in                    New   york  Ciiiiiity  now ");
+   int length=Integer.MAX_VALUE;
+   String result=null;
+     while (st.hasMoreTokens()) {
+    	 String s=st.nextToken();
+    	 if(s.length()<length) {
+    	  result= s;
+          length=result.length();
+         
+    	 }
+    	 
+    	 
+     }
+     
+     System.out.println(result);  
+   }  
+}  
